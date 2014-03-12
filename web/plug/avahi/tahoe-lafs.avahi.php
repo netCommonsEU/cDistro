a1ac62798e2b8e85d1b16cf9c97327da54790a26
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2014-03-12 18:07:08 +0100

    Change buttons for Tahoe-LAFS in Avahi search

--- web/plug/avahi/tahoe-lafs.avahi.php
+++ web/plug/avahi/tahoe-lafs.avahi.php
@@ -6,5 +6,5 @@ addAvahi('tahoe-lafs','ftahoeintroducer');
 function ftahoeintroducer($dates){
 	global $staticFile;
 
-	return ("<a class='btn' href='".$staticFile."/tahoe-lafs/node/".$dates['ip']."/".$dates['port']."'>Add to</a> <a class='btn' href='http://".$dates['ip'].":8228' target='_blank'>View</a> ");
+	return ("<a class='btn' href='http://".$dates['ip'].":8228' target='_blank'>View</a> <a class='btn' href='".$staticFile."/tahoe-lafs/createNode/".$dates['ip']."/".$dates['port']."'>Join grid</a>  ");
 }
