abc81e1c307ae5879e099cc25d4a8b04628f0431
Agustí Moll <agusti.moll@guifi.net>
2014-06-25 03:23:48 +0200

    Change function call when user joins to video

--- web/plug/avahi/peerstreamer.avahi.php
+++ web/plug/avahi/peerstreamer.avahi.php
@@ -6,5 +6,5 @@ addAvahi('peerstreamer','fpeerstreamer');
 function fpeerstreamer($dates){
 	global $staticFile;
 
-	return ("<a class='btn' href='".$staticFile."/peerstreamer/runps" . "?ip=" . $dates['ip'] ."&port=".$dates['port']."'>Join Video</a>  ");
+	return ("<a class='btn' href='".$staticFile."/peerstreamer/connect" . "?ip=" . $dates['ip'] ."&port=".$dates['port']."'>Join Video</a>  ");
 }
