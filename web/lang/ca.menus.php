4baccbc0df134942cbba25ccf57c70ba9ae7fcd0
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2014-09-08 18:36:04 +0200

    Translate menus (Catalan, Spanish)

---
+++ web/lang/ca.menus.php
@@ -0,0 +1,45 @@
+<?php
+// plug/lang/*.menu.php
+
+//Avahi
+
+addS ("menus_avahi_search","Cercar");
+addS ("menus_avahi_avahi","Avahi");
+
+
+//Cloudy
+addS ("menus_cloudy_system","Sistema");
+addS ("menus_cloudy_updates","Actualitzacions");
+addS ("menus_cloudy_sshkeys","Afegir claus SSH desenvolupadors");
+addS ("menus_cloudy_logout","Sortir");
+
+
+//Getinconf
+addS ("menus_getinconf_clommunity","Clommunity");
+addS ("menus_getinconf_getinconf","Getinconf");
+
+
+//Guifi.net
+addS ("menus_guifi_guifi","Guifi.net");
+addS ("menus_guifi_proxy3","Proxy3");
+addS ("menus_guifi_snpservices","SNPServices");
+addS ("menus_guifi_dnsservices","DNSServices");
+
+
+//Language
+addS ("menus_lang_language","Language");
+addS ("menus_lang_catalan","Català");
+addS ("menus_lang_english","Anglès");
+addS ("menus_lang_spanish","Castellà");
+
+
+//Tahoe-LAFS
+addS ("menus_tahoe_clommunity","Clommunity");
+addS ("menus_tahoe_tahoe","Tahoe-LAFS");
+
+
+//WebDAV
+addS ("menus_webdav_server", "Servidor WebDAV");
+addS ("menus_webdav_clommunity", "Clommunity");
+
+?>
---
+++ web/lang/es.menus.php
@@ -0,0 +1,45 @@
+<?php
+// plug/lang/*.menu.php
+
+//Avahi
+
+addS ("menus_avahi_search","Buscar");
+addS ("menus_avahi_avahi","Avahi");
+
+
+//Cloudy
+addS ("menus_cloudy_system","Sistema");
+addS ("menus_cloudy_updates","Actualizaciones");
+addS ("menus_cloudy_sshkeys","Añadir claves SSH desarrolladores");
+addS ("menus_cloudy_logout","Salir");
+
+
+//Getinconf
+addS ("menus_getinconf_clommunity","Clommunity");
+addS ("menus_getinconf_getinconf","Getinconf");
+
+
+//Guifi.net
+addS ("menus_guifi_guifi","Guifi.net");
+addS ("menus_guifi_proxy3","Proxy3");
+addS ("menus_guifi_snpservices","SNPServices");
+addS ("menus_guifi_dnsservices","DNSServices");
+
+
+//Language
+addS ("menus_lang_language","Language");
+addS ("menus_lang_catalan","Catalán");
+addS ("menus_lang_english","Inglés");
+addS ("menus_lang_spanish","Castellano");
+
+
+//Tahoe-LAFS
+addS ("menus_tahoe_clommunity","Clommunity");
+addS ("menus_tahoe_tahoe","Tahoe-LAFS");
+
+
+//WebDAV
+addS ("menus_webdav_server", "Servidor WebDAV");
+addS ("menus_webdav_clommunity", "Clommunity");
+
+?>
