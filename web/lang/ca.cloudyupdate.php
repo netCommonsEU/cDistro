e9870285477ffa1f577ab701fd0c93c4e69f69c4
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2014-09-08 18:44:45 +0200

    Typo

--- web/lang/ca.cloudyupdate.php
+++ web/lang/ca.cloudyupdate.php
@@ -5,7 +5,7 @@
 
 addS ("cloudyupdate_cloudy_packages","Paquets de Cloudy (aliens a Debian)");
 addS ("cloudyupdate_debian_packages","Paquets de Debian");
-addS ("cloudyupdate_update_debian_packages","Actualitzar els paquetes de Debian");
+addS ("cloudyupdate_update_debian_packages","Actualitza els paquets de Debian");
 addS ("cloudyupdate_loading_packages","Carregant la informació de paquets...");
 addS ("cloudyupdate_package","Paquet");
 addS ("cloudyupdate_installed_version","Versió instal·lada");
