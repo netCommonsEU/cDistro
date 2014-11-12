68f4eb5ffa9e7e774819684a9ec216b850161df2
Daniel Martí <mvdan@mvdan.cc>
2014-11-12 13:09:54 +0100

    Add syncthing to the menu

--- web/plug/menus/syncthing.menu.php
+++ web/plug/menus/syncthing.menu.php
@@ -1,3 +1,3 @@
 <?php
 //syncthing.menu.php
-//addMenu(t('menus_syncthing_server'),'syncthing',t('menus_syncthing_clommunity'));
+addMenu(t('menus_syncthing_server'),'syncthing',t('menus_syncthing_clommunity'));
