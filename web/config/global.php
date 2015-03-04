66617bec6e4c56baa99f02f7e07e2fb2610df0cc
Agustí Moll <agusti.moll@guifi.net>
2015-03-04 18:02:27 +0100

    [general]: Detect ssl protocol is exist

--- web/config/global.php
+++ web/config/global.php
@@ -1,5 +1,18 @@
 <?php
 //global.php
+$cdistro_conf = "/etc/cdistro.conf";
+
+$conf = parse_ini_file($cdistro_conf);
+list($wi_ip, $wi_port) = explode(":", $_SERVER['HTTP_HOST']);
+$protocol="http";
+
+if (isset($conf['PORT_SSL'])) {
+        if (($wi_port != $conf['PORT_SSL']) || ($_SERVER['REMOTE_ADDR'] != "127.0.0.1")){
+                header('Location: https://'.$wi_ip.':'.$conf['PORT_SSL']);
+        } else {
+                $protocol="https";
+        }
+}
 
 // You can change PATH files.
 $staticFile=$_SERVER['SCRIPT_NAME'];
@@ -13,7 +26,7 @@ $appCopyright = "&copy; ".$appCurrentYear.", GPLv2";
 $appHost = $_SERVER['HTTP_HOST'];
 $appHostname = gethostname();
 $appName = 'Cloudy';
-$appURL="http://".$appHost;
+$appURL=$protocol."://".$appHost;
 $sysCPU=`grep -m1 "model name" /proc/cpuinfo | awk -F: '{print $2}'`;
 $sysRAM=`free -h | grep Mem | awk '{print $2}'`."(".`grep -i "MemTotal" /proc/meminfo | awk -F: '{print $2}'`.")";
 $sysStorage=`df -h | grep -m 1 -e '/$' | awk '{ print $2 " / " $4 }'`;
