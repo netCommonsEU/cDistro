13296faa9ade8967fd68fa0b1dc0a708d7e34b71
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-03-11 18:17:07 +0100

    [guifi-web] Check login credentials with Guifi.net's API

--- web/lang/en.guifi-web.php
+++ web/lang/en.guifi-web.php
@@ -1,12 +1,29 @@
 <?php
 // plug/controllers/guifi-web.php
 
+//alerts
+addS ("guifi-web_alert_not_initialized","This node is not initialized");
+
+//buttons
+addS ("guifi-web_button_back","Back");
+addS ("guifi-web_button_initialize","Initialize node");
+addS ("guifi-web_button_submit_check","Check login credentials");
+
+
 //common
 addS ("guifi-web_common_title","Guifi.net website integration");
 
 //index
-addS ("guifi-web_index_subtitle","Integration of Cloudy nodes in Guifi.net website");
-addS ("guifi-web_index_description","Description 1");
+addS ("guifi-web_index_subtitle","Integration of Cloudy nodes with Guifi.net's website");
+addS ("guifi-web_index_description","This page lets you integrate your node with your user account at Guifi.net.");
+addS ("guifi-web_index_not_initialized","The configuration needed to integrate this node with Guifi.net's website is not yet initialized. Please click on the button below to start the process.");
 
-
+//initialize
+addS ("guifi-web_initialize_description",'To initialize the node you need to fill in your Guifi.net login credentials in the form below. If you do not have an account at Guifi.net\'s website you can <a href="http://guifi.net/en/user/register">create it here</a>.');
+addS ("guifi-web_initialize_form_password","Password");
+addS ("guifi-web_initialize_form_password_tooltip","Your passowrd at Guifi.net's website");
+addS ("guifi-web_initialize_form_username","Username");
+addS ("guifi-web_initialize_form_username_example","my_username_123");
+addS ("guifi-web_initialize_form_username_tooltip","Your username at Guifi.net's website");
+addS ("guifi-web_initialize_subtitle","Node initialization");
 ?>
--- web/plug/controllers/guifi-web.php
+++ web/plug/controllers/guifi-web.php
@@ -4,7 +4,7 @@
 $GUIFI_CONF_DIR = "/etc";
 $GUIFI_CONF_FILE = "guifi.conf";
 
-if (file_exists($GUIFI_CONF_DIR . '/' . $GUIFI_CONF_FILE))
+if (file_exists($GUIFI_CONF_DIR . '/' . $GUIFI_CONF_FILE) && !filesize($GUIFI_CONF_DIR . '/' . $GUIFI_CONF_FILE))
 	$GUIFI=load_conffile($GUIFI_CONF_DIR . '/' . $GUIFI_CONF_FILE);
 
 function index(){
@@ -18,8 +18,9 @@ function index(){
 
 	$page .= par(t("guifi-web_index_description"));
 
-	if (!file_exists($GUIFI_CONF_DIR . '/' . $GUIFI_CONF_FILE) || !file_exists($GUIFI_CONF_DIR . '/' . $GUIFI_CONF_FILE) ) {
+	if (!file_exists($GUIFI_CONF_DIR . '/' . $GUIFI_CONF_FILE) || !filesize($GUIFI_CONF_DIR . '/' . $GUIFI_CONF_FILE) ) {
 		$page .= "<div class='alert alert-warning text-center'>".t("guifi-web_alert_not_initialized")."</div>\n";
+		$page .= par(t("guifi-web_index_not_initialized"));
 		$buttons .= addButton(array('label'=>t("guifi-web_button_initialize"),'class'=>'btn btn-success', 'href'=>$staticFile.'/guifi-web/initialize'));
 	}
 
@@ -36,16 +37,58 @@ function initialize(){
 	$page .= hlc(t("guifi-web_common_title"));
 	$page .= hl(t("guifi-web_initialize_subtitle"),4);
 
-	if (!file_exists($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE))
+	$page .= par(t("guifi-web_initialize_description"));
+
+	$form = createForm(array('class'=>'form-horizontal'));
+	$form .= addInput('USERNAME',t("guifi-web_initialize_form_username"),'',array('type'=>'text','required'=>true),'',t("guifi-web_initialize_form_username_tooltip"));
+	$form .= addInput('PASSWORD',t("guifi-web_initialize_form_password"),'',array('type'=>'password', 'required'=>true),'',t("guifi-web_initialize_form_password_tooltip"));
+ 	$fbuttons = addSubmit(array('label'=>t('guifi-web_button_submit_check'),'class'=>'btn btn-primary'));
+
+	$page .= $form;
+
+	$buttons .= addButton(array('label'=>t("guifi-web_button_back"),'class'=>'btn btn-default', 'href'=>$staticFile.'/guifi-web'));
+	$buttons .= $fbuttons;
+
+	$page .= $buttons;
+	return(array('type' => 'render','page' => $page));
+}
+
+function initialize_post(){
+	global $staticFile, $GUIFI_CONF_DIR, $GUIFI_CONF_FILE;
+
+	$page = "";
+	$buttons = "";
+
+	$page .= hlc(t("guifi-web_common_title"));
+	$page .= hl(t("guifi-web_initialize_subtitle"),4);
+
+	/*if (!file_exists($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE))
 		touch($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE);
 	if (fileperms($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE) != "16877" )
 		chmod($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE, 0755);
+	*/
+	/*if (!file_exists($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE))
+		touch($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE);
+	if (fileperms($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE) != "16877" )
+		chmod($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE, 0755);
+	*/
 
+		$buttons .= addSubmit(array('label'=>t('tahoe-lafs_button_create_introducer'),'class'=>'btn btn-success'));
 
-	if (! file_exists($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE) || ( fileperms($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE) != "16877" ))
+	/*if (! file_exists($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE) || ( fileperms($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE) != "16877" ))
 		$page .= "<div class='alert alert-error text-center'>".t("guifi-web_alert_config_failed")."</div>\n";
-	elseif (! file_exists($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE) || ( fileperms($GUIFI_CONF_DIR) != "16877" )))
-		}
+	elseif (! file_exists($GUIFI_CONF_DIR.'/'.$GUIFI_CONF_FILE) || ( fileperms($GUIFI_CONF_DIR) != "16877" ))
+
+	*/
+
+	$page .= par("aa");
+	$page .= par($_POST['USERNAME']);
+	$page .= par(md5($_POST['PASSWORD']));
+	$page .= par("bb");
+
+	$content = file_get_contents("http://test.guifi.net/api?command=guifi.auth.login&username=".$_POST['USERNAME']."&password=".$_POST['PASSWORD']);
+		$page .= par($content);
+
 //	$result_dir = (shell_exec($cmd));
 
 /*
