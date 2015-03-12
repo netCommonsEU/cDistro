ff974a64faea9242b812fa9bac3b1e9a04c53aaa
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-03-12 21:05:01 +0100

    [guifi-web] Check node on Guifi API [WiP]

--- web/lang/en.guifi-web.php
+++ web/lang/en.guifi-web.php
@@ -2,7 +2,10 @@
 // plug/controllers/guifi-web.php
 
 //alerts
-addS ("guifi-web_alert_not_initialized","This node is not initialized");
+addS ("guifi-web_alert_index_nodeid","This node is registered at Guifi.net");
+addS ("guifi-web_alert_index_no_nodeid","This node is not registered at Guifi.net");
+addS ("guifi-web_alert_index_not_initialized","This node is not initialized");
+addS ("guifi-web_alert_index_initialized","This node is initialized");
 addS ("guifi-web_alert_initialize_curl_empty","Empty reply from remote server at Guifi.net");
 addS ("guifi-web_alert_initialize_curl_ok","User successfully authenticated");
 addS ("guifi-web_alert_initialize_curl_error","An error occurred");
@@ -11,7 +14,7 @@ addS ("guifi-web_alert_initialize_curl_wrong_command","Wrong command");
 addS ("guifi-web_alert_initialize_file_error","The configuration file could not be created");
 addS ("guifi-web_alert_initialize_file_empty","The configuration file is empty");
 addS ("guifi-web_alert_initialize_file_different","The configuration was not properly saved");
-addS ("guifi-web_alert_initialize_success","The node was successfully initialized");
+addS ("guifi-web_alert_initialize_success","The device was successfully initialized");
 addS ("guifi-web_alert_initialize_post_empty","The credentials form did not provide any data");
 addS ("guifi-web_alert_initialize_post_emptyusername","The credentials form did not provide a username");
 addS ("guifi-web_alert_initialize_post_emptypassword","The credentials form did not provide a password");
@@ -19,7 +22,8 @@ addS ("guifi-web_alert_initialize_post_emptypassword","The credentials form did 
 //buttons
 addS ("guifi-web_button_back","Back");
 addS ("guifi-web_button_back_credentials","Back to the credentials form");
-addS ("guifi-web_button_initialize","Initialize node");
+addS ("guifi-web_button_initialize","Initialize the device");
+addS ("guifi-web_button_register","Register the device");
 addS ("guifi-web_button_submit_check","Check login credentials");
 
 
@@ -27,12 +31,14 @@ addS ("guifi-web_button_submit_check","Check login credentials");
 addS ("guifi-web_common_title","Guifi.net website integration");
 
 //index
-addS ("guifi-web_index_subtitle","Integration of Cloudy nodes with Guifi.net's website");
-addS ("guifi-web_index_description","This page lets you integrate your node with your user account at Guifi.net.");
-addS ("guifi-web_index_not_initialized","The configuration needed to integrate this node with Guifi.net's website is not yet initialized. Please click on the button below to start the process.");
+addS ("guifi-web_index_description","This page lets you integrate your Cloudy-based device with your user account at Guifi.net.");
+addS ("guifi-web_index_not_initialized","The configuration needed to integrate this Cloudy device with Guifi.net's website is not yet initialized. Please click on the button below to start the process.");
+addS ("guifi-web_index_not_registered","This Cloudy device is initialized but is not yet registered to Guifi.net's website. Please click on the button below to start the process.");
+addS ("guifi-web_index_status","Cloudy device status:");
+addS ("guifi-web_index_subtitle","Integration of Cloudy devices with Guifi.net's website");
 
 //initialize
-addS ("guifi-web_initialize_description",'To initialize the node you need to fill in your Guifi.net login credentials in the form below. If you do not have an account at Guifi.net\'s website you can <a href="http://guifi.net/en/user/register">create it here</a>.');
+addS ("guifi-web_initialize_description",'To initialize this Cloudy device you need to fill in your Guifi.net login credentials in the form below. If you do not have an account at Guifi.net\'s website you can <a href="http://guifi.net/en/user/register">create it here</a>.');
 addS ("guifi-web_initialize_curl_empty","The remote server at Guifi.net could not be contacted, or it provided an empty reply. Please check your Internet connection and retry.");
 addS ("guifi-web_initialize_curl_error",'The remote server reported an error. Please go back to the previous form to retry.');
 addS ("guifi-web_initialize_curl_authresult","Authentication result:");
@@ -40,7 +46,7 @@ addS ("guifi-web_initialize_curl_details","Response details:");
 addS ("guifi-web_initialize_curl_url","URL called:");
 addS ("guifi-web_initialize_curl_wrong_command",'A wrong command was sent to the remote server. Please go back to the previous form to retry.');
 addS ("guifi-web_initialize_curl_wrong_login",'Please go back to the previous form to retry, or <a href="http://guifi.net/en/user/password">click here</a> to reset your password.');
-addS ("guifi-web_initialize_result","Node initialization result:");
+addS ("guifi-web_initialize_result","Device initialization result:");
 addS ("guifi-web_initialize_file_error","The configuration file could not be created. Check if you have free space in disk and sufficient permissions and retry.");
 addS ("guifi-web_initialize_file_empty","The configuration file was created but could not be written. Check if you have free space in disk and sufficient permissions and retry.");
 addS ("guifi-web_initialize_file_different","The configuration file was created but the data was not successfully saved. Check if you have free space in disk and sufficient permissions and retry.");
@@ -52,5 +58,11 @@ addS ("guifi-web_initialize_form_username_tooltip","Your username at Guifi.net's
 addS ("guifi-web_initialize_post_empty","The credentials form did not provide any data. Please go back to the previous page and retry.");
 addS ("guifi-web_initialize_post_emptyusername","The credentials form did not provide a username, or it was empty. Please go back to the previous page and retry.");
 addS ("guifi-web_initialize_post_emptypassword","The credentials form did not provide a password, or it was empty. Please go back to the previous page and retry.");
-addS ("guifi-web_initialize_subtitle","Node initialization");
+addS ("guifi-web_initialize_subtitle","Cloudy device initialization");
+
+//register
+addS ("guifi-web_register_subtitle","Cloudy device registration");
+addS ("guifi-web_register_description","To register this device you need to fill in the fields of the form below.");
+addS ("guifi-web_register_form_nodeid","Guifi node ID");
+addS ("guifi-web_register_form_nodeid_tooltip","The ID of the Guifi.net node this Cloudy device belongs to.");
 ?>
--- web/plug/controllers/guifi-web.php
+++ web/plug/controllers/guifi-web.php
@@ -15,14 +15,63 @@ function index(){
 
 	$page .= par(t("guifi-web_index_description"));
 
+	$page .= txt(t("guifi-web_index_status"));
 	if (!file_exists($GUIFI_CONF_DIR . '/' . $GUIFI_CONF_FILE) || !filesize($GUIFI_CONF_DIR . '/' . $GUIFI_CONF_FILE) ) {
-		$page .= "<div class='alert alert-warning text-center'>".t("guifi-web_alert_not_initialized")."</div>\n";
+		$page .= "<div class='alert alert-warning text-center'>".t("guifi-web_alert_index_not_initialized")."</div>\n";
 		$page .= par(t("guifi-web_index_not_initialized"));
 		$buttons .= addButton(array('label'=>t("guifi-web_button_initialize"),'class'=>'btn btn-success', 'href'=>$staticFile.'/guifi-web/initialize'));
 	}
 
+	else {
+		$GUIFI=load_conffile($GUIFI_CONF_DIR . '/' . $GUIFI_CONF_FILE);
+		$page .= "<div class='alert alert-success text-center'>".t("guifi-web_alert_index_initialized")."</div>\n";
+
+		if ( $GUIFI['NODE_ID'] ) {
+			$page .= "<div class='alert alert-success text-center'>".t("guifi-web_alert_index_nodeid")."</div>\n";
+
+		}
+
+		else {
+			$page .= "<div class='alert alert-warning text-center'>".t("guifi-web_alert_index_no_nodeid")."</div>\n";
+			$page .= par(t("guifi-web_index_not_registered"));
+			$buttons .= addButton(array('label'=>t("guifi-web_button_register"),'class'=>'btn btn-success', 'href'=>$staticFile.'/guifi-web/register'));
+		}
+/*
+
+
+		$gapi = new guifiAPI( $GUIFI['USERNAME'], '', $GUIFI['TOKEN']);
+
+		print_r($gapi);
+
+		$title = "Plaça Major, 34";
+		$zone_id = 27182;
+		$lat = '42.187065853813635';
+		$lon = '2.0233726501464844';
+		$node = array();
+		$node['nick'] = 'TEST34';
+		$node['body'] = "Node creat amb la API.";
+		$node['lat'] = '41.54301946112854'; // Latitude of the node
+		$node['lon'] = '1.5803146362304688'; // Longitude of the node
+		$node['elevation'] = 30; // 30 metres high
+		$node['stable'] = 'Yes';
+		$node['graph_server'] = 15902;
+		$node['status'] = 'Planned';
+
+		//$added = $gapi->addNode( $title, $zone_id, $lat, $lon, $node );
+		if( $added ) {
+			print_r( "Node created correctly!!<br />\n<br />\nThe identificator of the new node is: node_id = <strong>".$added->node_id."</strong>");
+		} else {
+			print_r("There was an error adding the node.<br />\n");
+			echo $gapi->getErrorsStr();
+		}
+
+		*/
+	}
+
 	$page .= $buttons;
 	return(array('type' => 'render','page' => $page));
+
+
 }
 
 function initialize(){
@@ -189,6 +238,79 @@ function initialize_post(){
 
 
 }
+
+function register(){
+	global $staticFile, $GUIFI_CONF_DIR, $GUIFI_CONF_FILE;
+
+	$page = "";
+	$buttons = "";
+
+	$page .= hlc(t("guifi-web_common_title"));
+	$page .= hl(t("guifi-web_register_subtitle"),4);
+
+	$page .= par(t("guifi-web_register_description"));
+
+	$form = createForm(array('class'=>'form-horizontal'));
+	$form .= addInput('NODE_ID',t("guifi-web_register_form_nodeid"),'',array('type'=>'number','required'=>true,'min'=>1),'',t("guifi-web_register_form_nodeid_tooltip"));
+
+ 	$fbuttons = addSubmit(array('label'=>t('guifi-web_button_submit_nodeid'),'class'=>'btn btn-primary'));
+
+	$page .= $form;
+
+	$buttons .= addButton(array('label'=>t("guifi-web_button_back"),'class'=>'btn btn-default', 'href'=>$staticFile.'/guifi-web'));
+	$buttons .= $fbuttons;
+
+	$page .= $buttons;
+	return(array('type' => 'render','page' => $page));
+}
+
+
+function register_post(){
+	global $staticFile, $GUIFI_CONF_DIR, $GUIFI_CONF_FILE;
+
+	$page = "";
+	$buttons = "";
+
+	$page .= hlc(t("guifi-web_common_title"));
+	$page .= hl(t("guifi-web_register_subtitle"),4);
+
+	if (empty($_POST)) {
+		$page .= "<div class='alert alert-error text-center'>".t("guifi-web_alert_register_post_empty")."</div>\n";
+		$page .= par(t("guifi-web_register_post_empty"));
+		$buttons .= addButton(array('label'=>t("guifi-web_button_back_register"),'class'=>'btn btn-default', 'href'=>$staticFile.'/guifi-web/register'));
+	}
+
+	else if (empty($_POST['NODE_ID'])) {
+		$page .= "<div class='alert alert-error text-center'>".t("guifi-web_alert_initialize_post_emptynodeid")."</div>\n";
+		$page .= par(t("guifi-web_initialize_post_emptynodeid"));
+		$buttons .= addButton(array('label'=>t("guifi-web_button_back_register"),'class'=>'btn btn-warning', 'href'=>$staticFile.'/guifi-web/register'));
+	}
+
+	else {
+		$GUIFI=load_conffile($GUIFI_CONF_DIR . '/' . $GUIFI_CONF_FILE);
+		/*$gapi = new guifiAPI( $GUIFI['USERNAME'], '', $GUIFI['TOKEN']);
+
+		$page .= ptxt(print_r($gapi,true));
+
+		$node_id = $_POST['NODE_ID'];
+
+		$updated = $gapi->updateNode( $node_id );
+		if( $updated ) {
+
+			$page .= ptxt(print_r( $gapi, true));
+		} else {
+			$page .= ptxt(print_r( $gapi->getErrorsStr(), true));
+		}
+		*/
+
+	}
+
+	$page .= $buttons;
+	return(array('type' => 'render','page' => $page));
+
+
+}
+
 
 function ffileperms(){
 print_r(fileperms("/etc"));
