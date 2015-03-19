10f95521fa6fb2b070dab67344f9a63d4284152f
Agustí Moll <agusti.moll@guifi.net>
2015-03-19 19:53:44 +0100

    [SSL]: Can active/desactive https version

---
+++ web/lang/ca.ssl.php
@@ -0,0 +1,11 @@
+<?php
+
+addS("menus_cloudy_ssl", "Gestor de SSL");
+addS("ssl_title", "Gestor de HTTPS");
+addS("ssl_desc", "Des de aquest panell pots canviar la seguretat de l'interficie web. [Consell]: Usa https sempre que puguis :-)")
+addS("https_is_not_installed","HTTPS no est&aacute; instal·lat");
+addS("ssl_install","Instal·lar SSL");
+addS("https_is_installed","HTTPS est&aacute; instal·lat");
+addS("ssl_remove","Desinstal·lar SSL");
+addS("ssl_is_installed_yet","HTTPS ja est&aacute; instal·lat");
+addS("ssl_is_not_installed_yet","HTTPS no est&aacute; instal·lat");
--- web/plug/menus/cloudy.menu.php
+++ web/plug/menus/cloudy.menu.php
@@ -2,4 +2,5 @@
 
 addMenu(t('menus_cloudy_updates'),'cloudyupdate',t('menus_cloudy_system'),'left');
 addMenu(t('menus_cloudy_sshkeys'),'sshkeys',t('menus_cloudy_system'),'left');
+addMenu(t('menus_cloudy_ssl'),'ssl',t('menus_cloudy_system'),'left');
 addMenu(t('menus_cloudy_logout'),'logout',t('menus_cloudy_system'),'left');
