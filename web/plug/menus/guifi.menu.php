b70cfef70ffabbe1dd2819c553b3372da90c9553
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-04-21 17:54:21 +0200

    [guifi-proxy3] Remove old Guifi Proxy3 entry from the menu

--- web/plug/menus/guifi.menu.php
+++ web/plug/menus/guifi.menu.php
@@ -3,6 +3,5 @@
 
 addMenu(t('menus_guifi_web'),'guifi-web',t('menus_guifi_guifi'));
 addMenu(t('menus_guifi_proxy3'),'guifi-proxy3',t('menus_guifi_guifi'));
-addMenu(t('menus_guifi_proxy3_old'),'guifi/proxy3',t('menus_guifi_guifi'));
 addMenu(t('menus_guifi_snpservices'),'guifi/snpservices',t('menus_guifi_guifi'));
 addMenu(t('menus_guifi_dnsservices'),'dnsservices',t('menus_guifi_guifi'));
