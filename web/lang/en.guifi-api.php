d6c58b7d766d8fbeec2613a3215edae39a7f5e6e
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-04-27 17:19:35 +0200

    [guifi-api] Add English translation

---
+++ web/lang/en.guifi-api.php
@@ -0,0 +1,33 @@
+<?php
+// lib/guifi_api.php
+
+//alerts
+addS ("guifi-api_alert_error","An error occurred calling Guifi.net API");
+addS ("guifi-api_alert_error_501","Insufficient permissions");
+
+
+//_handle_error
+addS ("guifi-api_handle_error_call_result:","Call to Guifi.net API result:");
+addS ("guifi-api_handle_error_details:","Error details:");
+addS ("guifi-api_handle_error_found:","Error found:");
+addS ("guifi-api_handle_error_501","The Guifi.net API returned an error with code 501. You don't have permission to perform the requested action. Check that you have the required permissions to perform the requested action or log in with another username with enough rights.");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+addS ("","");
+
+?>
