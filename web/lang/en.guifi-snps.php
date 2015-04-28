10b016e12a2d7c7a47fb45528043160ad2be348d
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-04-28 14:05:59 +0200

    [guifi-snps] Update English translation

--- web/lang/en.guifi-snps.php
+++ web/lang/en.guifi-snps.php
@@ -3,11 +3,71 @@
 
 //SNPServices
 
+//Alerts
+addS ("guifi-snps_alert_installed_pre","");
+addS ("guifi-snps_alert_installed_post"," is installed");
+addS ("guifi-snps_alert_not_installed_pre","");
+addS ("guifi-snps_alert_not_installed_post"," is not installed");
+addS ("guifi-snps_alert_running_pre","");
+addS ("guifi-snps_alert_running_post"," is running");
+addS ("guifi-snps_alert_stopped_pre","");
+addS ("guifi-snps_alert_stopped_post"," is stopped");
+addS ("guifi-snps_alert_not_guifi","This Cloudy device is not registered with Guifi.net's website");
+addS ("guifi-snps_alert_guifi","This Cloudy device is registered with Guifi.net's website");
+addS ("guifi-snps_alert_save","Saving and applying configuration...");
+
+//Common
+addS ("guifi-snps_common_appname","Guifi SNPServices");
+addS ("guifi-snps_common_status_pre","");
+addS ("guifi-snps_common_status_post"," status:");
+addS ("guifi-snps_common_guifi:","Guifi.net website integration:");
+addS ("guifi-snps_common_desc","This application provides monitorization and graphing tools for Guifi.net nodes");
+
+//Index
+addS ("guifi-snps_index_subtitle","Monitorization and graphing tools for Guifi.net nodes");
+addS ("guifi-snps_index_description1","SNPServices is a set of tools to capture the status (uptime, bandwidth usage, etc.) of the Guifi.net network nodes of a zone.");
+addS ("guifi-snps_index_description2","The web server at www.guifi.net tells each SNPServices server which nodes to monitor and store information about.");
+addS ("guifi-snps_index_description3","These data graphs are then visible to users on the Guifi.net website.");
+addS ("guifi-snps_index_connected","To run this service, the device has to be connected to both Guifi and the Internet.");
+addS ("guifi-snps_index_checkwiki","You can check this wiki page for more information:");
+addS ("guifi-snps_index_wikiurl","http://en.wiki.guifi.net/wiki/Graphs_Server");
+addS ("guifi-snps_index_not_guifi","This Cloudy device is not linked with any device at Guifi.net's website");
+addS ("guifi-snps_index_register_before","Click on the button below to register it before installing the SNPServices service.");
+addS ("guifi-snps_index_register","Click on the button below to register it.");
+addS ("guifi-snps_index_guifi","Click on the button below to install the SNPServices service.");
+
+
+//Install
+addS ("guifi-snps_install_subtitle","Installation and configuration");
+addS ("guifi-snps_install_declare","To install this service you need to declare it at Guifi.net's website to obtain a service ID.");
+addS ("guifi-snps_install_autodeclare","Click on the button below to automatically create the service at Guifi.net's website.");
+addS ("guifi-snps_install_otherwise","Otherwise, fill in the form click on the button to install the service without declaring it.");
+addS ("guifi-snps_install_declared_pre","To install ");
+addS ("guifi-snps_install_declared_post",", fill in the form click on the button below.");
+addS ("guifi-snps_install_configure_pre","Use this page to change ");
+addS ("guifi-snps_install_configure_post"," configuration or to uninstall it.");
+addS ("guifi-snps_install_value","The service ID has been autocompleted with the information retrieved from Guifi.net's website.");
+
+
+//Buttons
 addS ("guifi-snps_button_create_service","Create SNPServices at Guifi.net website");
 addS ("guifi-snps_button_register","Register this Cloudy device with Guifi.net website");
-addS ("guifi-snps_button_unregistered","Install SNPServices without registering this Cloudy device");
-addS ("guifi-snps_button_unregistereds","Install SNPServices without declaring it at Guifi.net website");
-addS ("guifi-snps_create_service","This Cloudy device does not have the SNPServices service declared at Guifi.net's website. Click on the button below to create it automatically before actually installing the device.");
-addS ("guifi-snps_register_cloudy","This Cloudy device is not linked with any device at Guifi.net's website. Click on the button below to register it before installing the SNPServices service");
+addS ("guifi-snps_button_back","Back");
+addS ("guifi-snps_button_install_pre","Install ");
+addS ("guifi-snps_button_install_post","");
+addS ("guifi-snps_button_sinstall_pre","Save settings and install ");
+addS ("guifi-snps_button_sinstall_post","");
+addS ("guifi-snps_button_save_pre","Save and apply ");
+addS ("guifi-snps_button_save_post"," settings");
+addS ("guifi-snps_button_manage_pre","Manage ");
+addS ("guifi-snps_button_manage_post","");
+addS ("guifi-snps_button_unregistered_pre","Install ");
+addS ("guifi-snps_button_unregistered_post"," without registering this device");
+addS ("guifi-snps_button_unregistereds_pre","Install ");
+addS ("guifi-snps_button_unregistereds_post"," without declaring it website");
+
+//Form
+addS ("guifi-snps_form_id","Service ID");
+addS ("guifi-snps_form_id_tooltip","The ID of the service at Guifi.net's website (e.g. http://guifi.net/node/<strong>123456</strong>)");
 
 ?>
