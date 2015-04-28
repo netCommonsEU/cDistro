2948916936111584a3291a4a32ef1a3b54b879ef
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-04-28 14:10:05 +0200

    Update English, Catalan and Spanish translations

--- web/lang/en.php
+++ web/lang/en.php
@@ -6,6 +6,7 @@
 addS ('Catalan','Catalan');
 addS ('English','English');
 addS ('Spanish','Spanish');
+
 // plug/controller/default.php
 addS ("Welcome!","Welcome!");
 addS ("Define language to: ", "Define language to: ");
@@ -21,6 +22,24 @@ addS ("package, please wait!","package, please wait!");
 addS ("System info","System info");
 addS ("Address","Address");
 addS ("Hostname","Hostname");
+addS ("default_button_back","Back");
+addS ("default_button_home","Home");
+addS ("default_generic_I_U_pre_Install","Installing package ");
+addS ("default_generic_I_U_post_Install",", please wait...");
+addS ("default_generic_I_U_pre_Uninstall","Uninstalling package ");
+addS ("default_generic_I_U_post_Uninstall",", please wait...");
+addS ("default_realInstall_result","Installation process result:");
+addS ("default_realInstall_details","Installation process details:");
+addS ("default_realInstall_installed_pre","Package ");
+addS ("default_realInstall_installed_post"," is installed");
+addS ("default_realInstall_not_installed_pre","Package ");
+addS ("default_realInstall_not_installed_post"," is not installed");
+addS ("default_realUninstall_result","Uninstallation process result:");
+addS ("default_realUninstall_details","Uninstallation process details:");
+addS ("default_realUninstall_installed_pre","Package ");
+addS ("default_realUninstall_installed_post"," is installed");
+addS ("default_realUninstall_not_installed_pre","Package ");
+addS ("default_realUninstall_not_installed_post"," is not installed");
 
 // plug/controllers/getinconf.php
 addS ('Getinconf Server URL','Getinconf Server URL');
@@ -57,8 +76,13 @@ addS ('Url from DNSDataServer (without ending backslash)','Url from DNSDataServe
 addS ("Guifi DNSServices","Guifi DNSServices");
 
 // lib/view.php
-addS ('This package is not yet installed','This package is not yet installed:');
-addS ('Install','Install');
+addS ("lib-view_button_back","Back");
+addS ("lib-view_button_install_pre","Install package ");
+addS ("lib-view_button_install_post","");
+addS ("lib-view_common_package_manager_title","Package manager");
+addS ("lib-view_common_package_manager_subtitle","Cloudy automated package installer and uninstaller");
+addS ('lib-view_package_not_installed_to_install','The following package is going to be installed:');
+addS ('lib-view_package_not_installed_text','Click on the button below to proceed with the installation. Required package dependencies will be automatically installed.');
 
 // plug/menus/avahi.php
 addS ('Search','Search');
