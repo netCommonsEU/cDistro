7eaf59726e571ef3d687a217bf9f8682e19ad897
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-05-04 18:55:27 +0200

    [default] Update Catalan l10n

---
+++ web/lang/ca.default.php
@@ -0,0 +1,36 @@
+<?php
+
+// web/plug/controllers/default.php
+
+//Common
+addS ("default_common_cloudy","Cloudy");
+addS ("default_common_description","El núvol per a xarxes comunitàries a les teves mans");
+
+//Buttons
+addS ("default_button_back","Enrere");
+addS ("default_button_home","Inici");
+
+//index
+addS ("default_index_systeminfo","Informació del dispositiu");
+addS ("default_index_address","Adreça i port: ");
+addS ("default_index_hostname","Nom del dispositiu: ");
+addS ("default_index_cpu","Processador: ");
+addS ("default_index_ram","Memòria RAM: ");
+addS ("default_index_storage","Emmagatzematge: ");
+
+addS ("default_generic_I_U_pre_Install","Instal·lant el paquet ");
+addS ("default_generic_I_U_post_Install",", si us plau, espereu...");
+addS ("default_generic_I_U_pre_Uninstall","Desinstalando el paquete ");
+addS ("default_generic_I_U_post_Uninstall",", si us plau, espereu...");
+addS ("default_realInstall_result","Resultat del procés d'instal·lació:");
+addS ("default_realInstall_details","Detalls del procés d'instal·lació:");
+addS ("default_realInstall_installed_pre","El paquet ");
+addS ("default_realInstall_installed_post"," està instal·lat");
+addS ("default_realInstall_not_installed_pre","El paquete ");
+addS ("default_realInstall_not_installed_post"," no està instal·lat");
+addS ("default_realUninstall_result","Resultat del procés de desinstal·lació:");
+addS ("default_realUninstall_details","Detalls del procés de desinstal·lació:");
+addS ("default_realUninstall_installed_pre","El paquet ");
+addS ("default_realUninstall_installed_post"," està instal·lat");
+addS ("default_realUninstall_not_installed_pre","El paquete ");
+addS ("default_realUninstall_not_installed_post"," no està instal·lat");
--- web/lang/ca.php
+++ web/lang/ca.php
@@ -1,5 +1,6 @@
 <?php
-//List of string english
+addLangFiles($documentPath . $lang_dir, "ca");
+
 // lib/errors.php
 
 // plug/menus/lang.menu.php
@@ -9,7 +10,6 @@ addS ('Spanish','Castellà');
 // lib/plug/controller/
 addS ('Welcome!','Benvingut !');
 addS ('Define language to: ', 'Idioma sel·leccionat: ');
-addS ('Configure your system!','Configura el sistema!');
 addS ('is already installed.','ja està instal·lat');
 addS ("isn't installed.","no està instal·lat.");
 addS ("Install","Instal·lar");
@@ -32,30 +32,7 @@ addS ("Now, service is loading. Please come back","Ara, el servei s'està activa
 addS ("previous page","pàgina anterior");
 addS ("You can return to the previous","Pots tornar a la pàgina anterior");
 
-addLangFiles($documentPath . $lang_dir, "ca");
 
-//Guifi
-addS ('Server URL Base','URL del Servidor');
-addS ('Guifi Proxy node number','Id del node Guifi Proxy');
-addS ('Main Server Ldap','Principal servidor Ldap');
-addS ('Main Server Ldap2','Principal servidor Ldap2');
-addS ('Proxy Welcome Message','Missatge de Benvinguda del Proxy');
-addS ('Contact email proxy server','Email de contacte del servidor proxy');
-addS ('Choosemanager a language for error pages generated',"Escull l'idioma dels missatges d'error.");
-addS ('Disk cache space (MB)','Espai de memòria cau (MB)');
-addS ('Ram cache space (MB)','Espai de RAM de memòria cau (MB)');
-addS ("This software provides a federated proxy service in the context of Guifi.net","Sistema Federa de Proxys a Guifi.net");
-addS ("Guifi Proxy3","Guifi Proxy3");
-
-addS ('Uninstall package','Desinstal·la el paquet');
-addS ("This software provides graphing services in the context of Guifi.net","Aquest programarí genera el servei de gràfiques a guifi.net");
-addS ('SNP Graph Server Id','Id del Servidor de grafiques SNP');
-addS ("Guifi SNPServices","Guifi SNPServices");
-
-addS ("This software provides DNS services in the context of Guifi.net","Aquest programarí genera el servei de DNS a guifi.net");
-addS ('Select your DNS Server Id to share your domains','Id del Servei DNS per compartir el teu domini');
-addS ('Url from DNSDataServer (without ending backslash)','Url de DNSDataServer (sense barra obliqua invertida al final)');
-addS ("Guifi DNSServices","Servei de DNS a Guifi");
 
 // lib/view.php
 addS ("lib-view_button_back","Torna");
@@ -66,23 +43,5 @@ addS ("lib-view_common_package_manager_subtitle","Instal·lador y desinstal·lad
 addS ("lib-view_package_not_installed_to_install","S'instal·larà el següent paquet:");
 addS ("lib-view_package_not_installed_text","Feu clic al botó per procedir amb la instal·lació. Les dependències del paquet s'instal·laran automàticament.");
 
-// plug/controller/default.php
-addS ("default_button_back","Enrere");
-addS ("default_button_home","Inici");
-addS ("default_generic_I_U_pre_Install","Instal·lant el paquet ");
-addS ("default_generic_I_U_post_Install",", si us plau, espereu...");
-addS ("default_generic_I_U_pre_Uninstall","Desinstalando el paquete ");
-addS ("default_generic_I_U_post_Uninstall",", si us plau, espereu...");
-addS ("default_realInstall_result","Resultat del procés d'instal·lació:");
-addS ("default_realInstall_details","Detalls del procés d'instal·lació:");
-addS ("default_realInstall_installed_pre","El paquet ");
-addS ("default_realInstall_installed_post"," està instal·lat");
-addS ("default_realInstall_not_installed_pre","El paquete ");
-addS ("default_realInstall_not_installed_post"," no està instal·lat");
-addS ("default_realUninstall_result","Resultat del procés de desinstal·lació:");
-addS ("default_realUninstall_details","Detalls del procés de desinstal·lació:");
-addS ("default_realUninstall_installed_pre","El paquet ");
-addS ("default_realUninstall_installed_post"," està instal·lat");
-addS ("default_realUninstall_not_installed_pre","El paquete ");
-addS ("default_realUninstall_not_installed_post"," no està instal·lat");
+
 
