4e4800da511c8f9249e4cda030836a9e85caccf8
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-05-04 16:53:32 +0200

    [guifi-snps] Update Spanish l10n

---
+++ web/lang/es.guifi-snps.php
@@ -0,0 +1,71 @@
+<?php
+// plug/controllers/guifi-snps.php
+
+//Common
+addS ("guifi-snps_common_appname","Guifi SNPServices");
+addS ("guifi-snps_common_status_pre","Estado de ");
+addS ("guifi-snps_common_status_post",":");
+addS ("guifi-snps_common_guifi:","Integración en la web de Guifi.net:");
+addS ("guifi-snps_common_desc","Esta aplicación proporciona herramientas de monitorización y gráficas para nodos de Guifi.net");
+
+//Alerts
+addS ("guifi-snps_alert_installed_pre","");
+addS ("guifi-snps_alert_installed_post"," está instalado");
+addS ("guifi-snps_alert_not_installed_pre","");
+addS ("guifi-snps_alert_not_installed_post"," no está instalat");
+addS ("guifi-snps_alert_running_pre","");
+addS ("guifi-snps_alert_running_post"," está ejecutándose");
+addS ("guifi-snps_alert_stopped_pre","");
+addS ("guifi-snps_alert_stopped_post"," está parado");
+addS ("guifi-snps_alert_not_guifi","Este dispositivo Cloudy no está registrado en la web de Guifi.net");
+addS ("guifi-snps_alert_guifi","Este dispositivo Cloudy está registrado en la web de Guifi.net");
+addS ("guifi-snps_alert_save","Se está guardando y aplicando la configuración...");
+
+//Buttons
+addS ("guifi-snps_button_create_service","Crear un servicio SNPServices en la web de Guifi.net");
+addS ("guifi-snps_button_register","Registrar este dispositivo Cloudy en la web de Guifi.net");
+addS ("guifi-snps_button_back","Atrás");
+addS ("guifi-snps_button_install_pre","Instalar ");
+addS ("guifi-snps_button_install_post","");
+addS ("guifi-snps_button_sinstall_pre","Guardar la configuración e instalar ");
+addS ("guifi-snps_button_sinstall_post","");
+addS ("guifi-snps_button_save","Guardar y aplicar la configuración");
+addS ("guifi-snps_button_uninstall_pre","Desinstalar ");
+addS ("guifi-snps_button_uninstall_post","");
+addS ("guifi-snps_button_manage_pre","Administrar ");
+addS ("guifi-snps_button_manage_post","");
+addS ("guifi-snps_button_unregistered_pre","Instalar ");
+addS ("guifi-snps_button_unregistered_post"," sin registrar el dispositivo");
+addS ("guifi-snps_button_unregistereds_pre","Instalar ");
+addS ("guifi-snps_button_unregistereds_post"," sin declarar el servicio en la web");
+
+//Index
+addS ("guifi-snps_index_subtitle","Herramientas de monitorización y gráficas para nodos de Guifi.net");
+addS ("guifi-snps_index_description1","SNPServices es un conjunto de herramientas para capturar el estado (tiempo en funcionamiento, uso de ancho de banda, etc.) de los nodos de Guifi.net de una zona");
+addS ("guifi-snps_index_description2","El servidor web en www.guifi.net indica a cada servidor SNPServices qué nodos debe monitorizar y guardar la información.");
+addS ("guifi-snps_index_connected","Per executar Este servei, el dispositivo ha d'estar connectat tant a Guifi.net com a Internet.");
+addS ("guifi-snps_index_description3","Estos datos y gráficas son visibles para los usuarios en la web de Guifi.net.");
+addS ("guifi-snps_index_checkwiki","Puede echar un vistazo a esta página para más información:");
+addS ("guifi-snps_index_wikiurl","http://es.wiki.guifi.net/wiki/Servidor_de_gráficas");
+addS ("guifi-snps_index_not_guifi","Este dispositivo Cloudy no está vinculado a ningún dispositivo en la web de Guifi.net.");
+addS ("guifi-snps_index_register_before","Haga clic en el botón a continuación para registrarlo antes de instalar el servicio  SNPServices.");
+addS ("guifi-snps_index_register","Haga clic en el botón a continuación para registrarlo.");
+addS ("guifi-snps_index_guifi_pre","Haga clic en el botón para instalar el servicio ");
+addS ("guifi-snps_index_guifi_post",".");
+
+//Install
+addS ("guifi-snps_install_subtitle","Instalación y configuración");
+addS ("guifi-snps_install_declare","Para instalar este servicio debe declararse en la web de Guifi.net para obtener un identificador (ID) del servicio.");
+addS ("guifi-snps_install_autodeclare","Haga clic en el botón a continuación para crear el servicio automáticamente en la web de Guifi.net.");
+addS ("guifi-snps_install_otherwise","Alternativamente, rellene los campos del formulario a continuación y haga clic en en botón para instalar el servicio sin declararlo.");
+addS ("guifi-snps_install_declared_pre","Para instalar ");
+addS ("guifi-snps_install_declared_post",", rellene el formulario a continuación y haga clic en el botón de debajo.");
+addS ("guifi-snps_install_configure_pre","Use esta página para cambiar la configuración del servicio ");
+addS ("guifi-snps_install_configure_post"," o para desinstalarlo.");
+addS ("guifi-snps_install_value","El identificador (ID) del servicio se ha autocompletado con la información obtenida de la web de Guifi.net.");
+
+//Form
+addS ("guifi-snps_form_id","ID del servicio");
+addS ("guifi-snps_form_id_tooltip","El identificador (ID) del servicio en la web de Guifi.net (e.g. http://guifi.net/node/<strong>123456</strong>)");
+
+?>
