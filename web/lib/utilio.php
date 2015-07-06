5adb07407d47b2828c7df7d9b8ff4ef888a790fe
Agustí Moll <agusti.moll@guifi.net>
2015-07-06 17:58:00 +0200

    [Global]: fixed this bug http://dev.cloudy.community/Guifi.net/web-interface/issues/38

--- web/lib/utilio.php
+++ web/lib/utilio.php
@@ -332,6 +332,7 @@ function _getHttp($url){
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	$resposta = curl_exec($ch);
 	curl_close($ch);
 
