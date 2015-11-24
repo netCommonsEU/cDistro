e373d1703460785a809e788b58abf83262b56b7f
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-11-24 14:22:23 +0100

    [settings] Add English texts

---
+++ web/lang/en.settings.php
@@ -0,0 +1,44 @@
+<?php
+// plug/lang/en.settings.php
+
+//Settings
+
+//alerts
+addS ("settings_alert_","");
+
+
+//buttons
+addS ("settings_button_hostname","Modify hostname");
+addS ("settings_button_back","Back");
+addS ("settings_button_shostname","Save hostname");
+addS ("settings_button_","");
+
+
+//common
+addS ("settings_common_title","System settings");
+addS ("settings_common_subtitle","System settings management and configuration");
+addS ("settings_common_","");
+
+
+//flash
+addS ("settings_flash_hostname","Hostname successfully changed");
+addS ("settings_flash_hostnamefail","Hostname change failed");
+addS ("settings_flash_","");
+
+
+//hostname
+addS ("settings_hostname_title","Hostname");
+addS ("settings_hostname_current","Current hostname:");
+addS ("settings_hostname_subtitle","Hostname modification");
+addS ("settings_hostname_description","Use this page to modify the hostname of this Cloudy device.");
+addS ("settings_hostname_fname","Hostname");
+addS ("settings_hostname_ftooltip","Write the new hostname for this device using lowercase or uppercase letters, numbers and hyphens. The hostname must start and finish with numbers or letters.");
+addS ("settings_hostname_fplaceholder","Enter a valid hostname");
+addS ("settings_hostname_invalid","Invalid hostname:");
+addS ("settings_hostname_empty","(empty)");
+
+//index
+addS ("settings_index_description","This page shows different system settings and lets you modify them.");
+addS ("settings_index_","");
+
+?>
