26e6d14eaf47ca921d1e8cdb357577222f544c1c
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-11-24 14:26:51 +0100

    [settings] Add Catalan texts

---
+++ web/lang/ca.settings.php
@@ -0,0 +1,44 @@
+<?php
+// plug/lang/ca.settings.php
+
+//Settings
+
+//alerts
+addS ("settings_alert_","");
+
+
+//buttons
+addS ("settings_button_hostname","Canvia el nom del dispositiu");
+addS ("settings_button_back","Enrere");
+addS ("settings_button_shostname","Desa el nom del dispositiu");
+addS ("settings_button_","");
+
+
+//common
+addS ("settings_common_title","Configuració del dispositiu");
+addS ("settings_common_subtitle","Gestió de la configuració i els paràmetres del dispositiu");
+addS ("settings_common_","");
+
+
+//flash
+addS ("settings_flash_hostname","Nom del dispositiu modificat correctament");
+addS ("settings_flash_hostnamefail","Ha fallat el canvi de nom del dispositiu");
+addS ("settings_flash_","");
+
+
+//hostname
+addS ("settings_hostname_title","Nom del dispositiu");
+addS ("settings_hostname_current","Nom actual del dispositiu:");
+addS ("settings_hostname_subtitle","Modificació del nom de dispositiu");
+addS ("settings_hostname_description","Empreu aquesta pàgina per modificar el nom d'aquest dispositiu amb Cloudy.");
+addS ("settings_hostname_fname","Nom:");
+addS ("settings_hostname_ftooltip","Escriviu el nou nom del dispositiu emprant majúscules o minúscules, lletres i guionets. El nom ha de començar i acabar amb un número o una lletra.");
+addS ("settings_hostname_fplaceholder","Introduiu un nom de dispositiu vàlid");
+addS ("settings_hostname_invalid","Nom de dispositiu invàlid:");
+addS ("settings_hostname_empty","(buit)");
+
+//index
+addS ("settings_index_description","Aquesta pàgina us mostra diferents paràmetres del sistema i us permet modificar-los.");
+addS ("settings_index_","");
+
+?>
