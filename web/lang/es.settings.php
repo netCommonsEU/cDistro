abbf052b53047cbd3590b4a301a46c8c75dca7a8
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2015-11-24 14:30:19 +0100

    [settings] Add hostname change Spanish texts

---
+++ web/lang/es.settings.php
@@ -0,0 +1,44 @@
+<?php
+// plug/lang/ca.settings.php
+
+//Settings
+
+//alerts
+addS ("settings_alert_","");
+
+
+//buttons
+addS ("settings_button_hostname","Cambia el nombre del dispositivo");
+addS ("settings_button_back","Volver");
+addS ("settings_button_shostname","Guarda el nombre del dispositivo");
+addS ("settings_button_","");
+
+
+//common
+addS ("settings_common_title","Configuración del dispositivo");
+addS ("settings_common_subtitle","Gestión de la configuración y los parámetros del dispositivo");
+addS ("settings_common_","");
+
+
+//flash
+addS ("settings_flash_hostname","Nombre del dispositivo modificado correctamente");
+addS ("settings_flash_hostnamefail","Ha ocurrido un error al cambiar el nombre del dispositivo");
+addS ("settings_flash_","");
+
+
+//hostname
+addS ("settings_hostname_title","Nombre del dispositivo");
+addS ("settings_hostname_current","Nombre actual del dispositivo:");
+addS ("settings_hostname_subtitle","Modificación del nombre de dispositivo");
+addS ("settings_hostname_description","Use esta página para modificar el nombre de este dispositivo Cloudy.");
+addS ("settings_hostname_fname","Nombre:");
+addS ("settings_hostname_ftooltip","Escriba el nombre del nuevo dispositivo usando mayúsculas y minúsculas, números y guiones. El nombre debe empezar y terminar con una letra o un número.");
+addS ("settings_hostname_fplaceholder","Introduzca un nombre de dispositivo válido");
+addS ("settings_hostname_invalid","Nombre de dispositivo inválido:");
+addS ("settings_hostname_empty","(vacío)");
+
+//index
+addS ("settings_index_description","Esta página le muestra diferentes parámetros del sistema y le permite modificarlos.");
+addS ("settings_index_","");
+
+?>
