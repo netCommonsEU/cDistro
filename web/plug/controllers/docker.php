8f57b4675a4c00583e43ff17f02cb6e98044e1ba
agusti <agusti.moll@guifi.net>
2016-02-19 12:45:22 +0100

    [docker]: install/uninstall

--- web/plug/controllers/docker.php
+++ web/plug/controllers/docker.php
@@ -12,32 +12,32 @@ function index() {
 		$page .= "<div class='alert alert-error text-center'>".t("docker_not_installed")."</div>\n";
 		$page .= addButton(array('label'=>t("docker_install"),'class'=>'btn btn-success', 'href'=>"$urlpath/install"));
 		return array('type'=>'render','page'=>$page);
-	} elseif (!isConfigured()) {
-		$page .= "<div class='alert alert-error text-center'>".t("docker_not_configured")."</div>\n";
-		$page .= addButton(array('label'=>t("docker_configure"),'class'=>'btn btn-success', 'href'=>"$urlpath/configure"));
-		$page .= addButton(array('label'=>t("docker_remove"),'class'=>'btn btn-danger', 'href'=>"$urlpath/remove"));
-		return array('type'=>'render','page'=>$page);
 	} elseif (!isRunning()) {
 		$page .= "<div class='alert alert-error text-center'>".t("docker_not_running")."</div>\n";
-		$page .= addButton(array('label'=>t("syncthing_start"),'class'=>'btn btn-success', 'href'=>"$urlpath/start"));
-		$page .= addButton(array('label'=>t("syncthing_remove"),'class'=>'btn btn-danger', 'href'=>"$urlpath/remove"));
+		$page .= addButton(array('label'=>t("docker_start"),'class'=>'btn btn-success', 'href'=>"$urlpath/start"));
+		$page .= addButton(array('label'=>t('docker_remove'),'class'=>'btn btn-danger', 'href'=>$staticFile.'/default/uninstall/'.$docker_pkg));
 		return array('type'=>'render','page'=>$page);
 	} else {
 		$page .= "<div class='alert alert-success text-center'>".t("docker_running")."</div>\n";
-		$page .= addButton(array('label'=>t("syncthing_stop"),'class'=>'btn btn-danger', 'href'=>"$urlpath/stop"));
+		$page .= addButton(array('label'=>t("docker_stop"),'class'=>'btn btn-danger', 'href'=>"$urlpath/stop"));
 
 		return array('type' => 'render','page' => $page);
 	}
 }
-function isConfigured() {
-  return true;
-}
 function isRunning(){
-  return true;
+	$cmd = "/usr/bin/docker ps &>/dev/null"
+	$ret = shell_exec($cmd);
+  return($ret['return'] ==  0 );
 }
 function install(){
   global $title, $urlpath, $docker_pkg;
 
   $page = package_not_install($docker_pkg,t("docker_desc"));
   return array('type' => 'render','page' => $page);
+}
+function start() {
+	execute_program_detached("service docker start");
+}
+function stop() {
+	execute_program_detached("service docker start");
 }
