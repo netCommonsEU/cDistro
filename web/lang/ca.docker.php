508f7aa7bee640692fe7515c375695714a64b885
Jorge L. Florit <jlflorit@gmail.com>
2016-04-06 12:46:28 +0200

    [Docker]: fixed language files.

--- web/lang/ca.docker.php
+++ web/lang/ca.docker.php
@@ -2,15 +2,14 @@
 
 //Common
 addS ("docker_title","Docker");
-addS ("docker_desc","Docker és un projecte de codi obert que automatitza el desplegament d'aplicacions dintre de contenidors de programari, proporcionant una capa adicional de abastracció i automatització de Virtualització a nivell de sistema operatiun.");
-
+addS ("docker_desc","Docker &eacute;s un projecte de codi obert que automatitza el desplegament d'aplicacions dintre de contenidors de programari, proporcionant una capa addicional d'abstracci&oacute; i automatitzaci&oacute; de virtualitzaci&oacute; a nivell de sistema operatiu.");
 
 addS ("docker_not_installed","Docker.io no est&agrave; instal·lat.");
 addS ("docker_install","Docker.io est&agrave; instal·lat.");
-addS ("docker_not_running","Docker.io esta parat.");
-addS ("docker_start","Activar Docker.io");
-addS ("docker_remove","Desintal·lar Docker.io");
-addS ("docker_running","Docker en funcionament");
-addS ("docker_stop","Desactivar Docker.io");
-addS ("docker_start_message","Docer.io arrancat.");
+addS ("docker_not_running","Docker.io est&agrave; parat.");
+addS ("docker_start","Activar Docker.io.");
+addS ("docker_remove","Desintal·lar Docker.io.");
+addS ("docker_running","Docker.io en funcionament.");
+addS ("docker_stop","Desactivar Docker.io.");
+addS ("docker_start_message","Docker.io arrancat.");
 addS ("docker_stop_message","Docker.io aturat.");
--- web/lang/es.docker.php
+++ web/lang/es.docker.php
@@ -2,14 +2,14 @@
 
 //Common
 addS ("docker_title","Docker");
-addS ("docker_desc",'Docker es un proyecto de código abierto que automatiza el despliegue de aplicaciones dentro de contenedores de software, proporcionando una capa adicional de abstracción y automatización de Virtualización a nivel de sistema operativo.');
+addS ("docker_desc",'Docker es un proyecto de c&oacute;digo abierto que automatiza el despliegue de aplicaciones dentro de contenedores de software, proporcionando una capa adicional de abstracci&oacute;n y automatizaci&oacute;n de virtualizaci&oacute;n a nivel de sistema operativo.'
 
-addS ("docker_not_installed","Docker.io no esta instalado.");
-addS ("docker_install","Docker.io esta instalado.");
-addS ("docker_not_running","Docker.io esta parado.");
-addS ("docker_start","Activar Docker.io");
+addS ("docker_not_installed","Docker.io no est&aacute; instalado.");
+addS ("docker_install","Docker.io est&aacute; instalado.");
+addS ("docker_not_running","Docker.io est&aacute; parado.");
+addS ("docker_start","Activar Docker.io.");
 addS ("docker_remove","Desinstalar Docker.io");
-addS ("docker_running","Docker en funcionamiento");
-addS ("docker_stop","Desactivar Docker.io");
-addS ("docker_start_message","Docer.io arrancar.");
-addS ("docker_stop_message","Docker.io parar.");
+addS ("docker_running","Docker.io en funcionamiento.");
+addS ("docker_stop","Desactivar Docker.io.");
+addS ("docker_start_message","Docker.io arrancado.");
+addS ("docker_stop_message","Docker.io parado.");
