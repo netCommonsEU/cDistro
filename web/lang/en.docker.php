9666d606cdf73a306504a2c9d1af48e112b2ab57
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2017-06-02 16:16:56 +0200

    [docker] Update English translation

--- web/lang/en.docker.php
+++ web/lang/en.docker.php
@@ -5,6 +5,9 @@
 addS ("docker_alert_installed","Docker is installed");
 addS ("docker_alert_not_installed","Docker is not installed");
 addS ("docker_alert_not_running","Docker is stopped");
+addS ("docker_alert_img_not_available","No Docker images available");
+addS ("docker_alert_ps_not_running","No running Docker containers");
+addS ("docker_alert_ps_not_stopped","No stopped Docker containers");
 addS ("docker_alert_running","Docker is running");
 addS ("docker_alert_start_message","Docker has been started");
 addS ("docker_alert_stop_message","Docker has been stopped");
@@ -17,14 +20,29 @@ addS ("docker_button_search","Search Docker containers");
 addS ("docker_button_start","Start Docker");
 addS ("docker_button_stop","Stop Docker");
 addS ("docker_button_uninstall","Uninstall Docker (docker.io)");
-addS ("docker_button_container_stop","Stop");
+addS ("docker_button_container_pull","Pull image");
+addS ("docker_button_container_stop","Stop container");
+addS ("docker_button_container_rm","Remove container");
+addS ("docker_button_container_restart","Restart container");
+addS ("docker_button_image_rmi","Remove image");
+addS ("docker_button_image_run","Run image");
+
 
 
 
 //flash
+addS ("docker_flash_restart_pre","Restarting container ");
+addS ("docker_flash_restart_mid"," (ID: ");
+addS ("docker_flash_restart_post",") in the background. This may take some seconds...");
 addS ("docker_flash_rm_pre","Removing container ");
 addS ("docker_flash_rm_mid"," (ID: ");
 addS ("docker_flash_rm_post",") in the background. This may take some seconds...");
+addS ("docker_flash_rmi_pre","Removing image ");
+addS ("docker_flash_rmi_mid","");
+addS ("docker_flash_rmi_post"," in the background. This may take some seconds...");
+addS ("docker_flash_run_pre","Launching image ");
+addS ("docker_flash_run_mid","");
+addS ("docker_flash_run_post"," in the background. This may take some seconds...");
 addS ("docker_flash_pull_pre","Pulling container ");
 addS ("docker_flash_pull_post"," in the background. This may take some seconds...");
 addS ("docker_flash_stop_pre","Stopping container ");
@@ -48,7 +66,7 @@ addS ("docker_search_desc",'Use the form below to search at <a href="https://hub
 addS ("docker_search_form_automated",'Automated');
 addS ("docker_search_form_automated_tooltip",'Search only for <a href="https://docs.docker.com/docker-cloud/builds/automated-build/">automatically built</a> Docker images.');
 addS ("docker_search_form_search",'Search text');
-addS ("docker_search_form_search_tooltip",'Type the name (or part of it) of a Docker container, a repository or an application to search for it.');
+addS ("docker_search_form_search_tooltip",'Type the name (or part of it) of a Docker container, a repository or an application to search for it. Use only lowercase and numbers.');
 addS ("docker_search_header_name",'Name');
 addS ("docker_search_header_description",'Description');
 addS ("docker_search_header_stars",'Stars');
@@ -62,6 +80,7 @@ addS ("docker_search_yes",'Yes');
 //main
 addS ("docker_info","Docker info:");
 addS ("docker_interface","Docker network interface:");
-addS ("docker_title_containers", "Running Docker containers:");
+addS ("docker_title_containers_running", "Running Docker containers:");
+addS ("docker_title_containers_stopped", "Stopped Docker containers:");
 addS ("docker_title_images", "Available Docker images:");
 
