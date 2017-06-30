2a3e6de31e9b46442343b5d8709ac3a9e69ae6c3
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2017-06-30 10:14:43 +0200

    [docker] Replace Docker.io naming with Docker CE

--- web/lang/ca.docker.php
+++ web/lang/ca.docker.php
@@ -4,15 +4,15 @@
 addS ("docker_title","Docker");
 addS ("docker_desc","Docker &eacute;s un projecte de codi obert que automatitza el desplegament d'aplicacions dintre de contenidors de programari, proporcionant una capa addicional d'abstracci&oacute; i automatitzaci&oacute; de virtualitzaci&oacute; a nivell de sistema operatiu.");
 
-addS ("docker_not_installed","Docker.io no est&agrave; instal·lat.");
-addS ("docker_installed","Docker.io est&agrave; instal·lat.");
-addS ("docker_button_install","Instal·la Docker (docker.io)");
-addS ("docker_not_running","Docker.io est&agrave; parat.");
-addS ("docker_start","Activar Docker.io.");
-addS ("docker_remove","Desintal·lar Docker.io.");
-addS ("docker_running","Docker.io en funcionament.");
-addS ("docker_stop","Desactivar Docker.io.");
-addS ("docker_start_message","Docker.io arrancat.");
-addS ("docker_stop_message","Docker.io aturat.");
+addS ("docker_not_installed","Docker no est&agrave; instal·lat.");
+addS ("docker_installed","Docker est&agrave; instal·lat.");
+addS ("docker_button_install","Instal·la Docker (Community Edition)");
+addS ("docker_not_running","Docker est&agrave; parat.");
+addS ("docker_start","Activar Docker.");
+addS ("docker_remove","Desintal·lar Docker.");
+addS ("docker_running","Docker en funcionament.");
+addS ("docker_stop","Desactivar Docker.");
+addS ("docker_start_message","Docker arrancat.");
+addS ("docker_stop_message","Docker aturat.");
 addS ("docker_info","Docker info:");
 addS ("docker_interface","Docker interf&iacute;cie de xarxa:");
--- web/lang/es.docker.php
+++ web/lang/es.docker.php
@@ -4,15 +4,15 @@
 addS ("docker_title","Docker");
 addS ("docker_desc",'Docker es un proyecto de c&oacute;digo abierto que automatiza el despliegue de aplicaciones dentro de contenedores de software, proporcionando una capa adicional de abstracci&oacute;n y automatizaci&oacute;n de virtualizaci&oacute;n a nivel de sistema operativo.');
 
-addS ("docker_not_installed","Docker.io no est&aacute; instalado.");
-addS ("docker_installed","Docker.io est&aacute; instalado.");
-addS ("docker_button_install","Instalar Docker (docker.io)");
-addS ("docker_not_running","Docker.io est&aacute; parado.");
-addS ("docker_start","Activar Docker.io.");
-addS ("docker_remove","Desinstalar Docker.io");
-addS ("docker_running","Docker.io en funcionamiento.");
-addS ("docker_stop","Desactivar Docker.io.");
-addS ("docker_start_message","Docker.io arrancado.");
-addS ("docker_stop_message","Docker.io parado.");
+addS ("docker_not_installed","Docker no est&aacute; instalado.");
+addS ("docker_installed","Docker est&aacute; instalado.");
+addS ("docker_button_install","Instalar Docker (Community Edition)");
+addS ("docker_not_running","Docker est&aacute; parado.");
+addS ("docker_start","Activar Docker.");
+addS ("docker_remove","Desinstalar Docker");
+addS ("docker_running","Docker en funcionamiento.");
+addS ("docker_stop","Desactivar Docker.");
+addS ("docker_start_message","Docker arrancado.");
+addS ("docker_stop_message","Docker parado.");
 addS ("docker_info","Docker info:");
 addS ("docker_interface","Docker interfaz de red:");
