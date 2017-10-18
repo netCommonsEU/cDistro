b8ed9190d724619f4d59dd5dba6f714a19f2cd39
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2017-10-18 08:46:30 +0200

    [docker] Update Spanish strings

--- web/lang/es.docker.php
+++ web/lang/es.docker.php
@@ -1,6 +1,5 @@
 <?php
 
-
 //alerts
 addS ("docker_addsources_available", "Docker está disponible para su instalación");
 addS ("docker_addsources_not_available", "Docker no está disponible para su instalación");
@@ -16,6 +15,7 @@ addS ("docker_alert_ps_not_stopped","No hay contenedores Docker parados");
 addS ("docker_alert_running","Docker está ejecutándose");
 addS ("docker_alert_start_message","Docker se ha iniciado");
 addS ("docker_alert_stop_message","Docker se ha parado");
+addS ("docker_alert_vol_none", "No hay volúmenes Docker disponibles");
 
 
 //buttons
@@ -29,15 +29,19 @@ addS ("docker_button_search","Buscar imágenes de Docker");
 addS ("docker_button_start","Iniciar Docker");
 addS ("docker_button_stop","Parar Docker");
 addS ("docker_button_uninstall","Desinstalar Docker (docker-ce)");
+addS ("docker_button_container_publish","Publicar");
 addS ("docker_button_container_pull","Obtener imagen");
 addS ("docker_button_container_stop","Parar contenedor");
 addS ("docker_button_container_rm","Borrar contenedor");
 addS ("docker_button_container_restart","Reiniciar contenedor");
+addS ("docker_button_container_unpublish","Despublicar");
 addS ("docker_button_image_rmi","Borrar imagen");
 addS ("docker_button_image_run","Iniciar imagen");
 addS ("docker_button_pdcontainer_config","Configurar");
 addS ("docker_button_pdcontainer_run","Iniciar");
 addS ("docker_button_pdform_run","Iniciar");
+addS ("docker_button_volume_inspect","Inspeccionar");
+addS ("docker_button_volume_rm","Eliminar");
 
 
 //flash
@@ -53,11 +57,15 @@ addS ("docker_flash_rmi_post"," en segundo plano. Puede tardar unos segundos");
 addS ("docker_flash_run_pre","Iniciando imagen ");
 addS ("docker_flash_run_mid","");
 addS ("docker_flash_run_post"," en segundo plano. Puede tardar unos segundos");
+addS ("docker_flash_publish_error_pre","No se ha podido publicar el contenedor ");
+addS ("docker_flash_publish_error_post","");
 addS ("docker_flash_pull_pre","Obteniendo imagen ");
 addS ("docker_flash_pull_post"," en segundo plano. Puede tardar unos segundos");
 addS ("docker_flash_stop_pre","Parando contenedor ");
 addS ("docker_flash_stop_mid"," (ID: ");
 addS ("docker_flash_stop_post",") en segundo plano. Puede tardar unos segundos");
+addS ("docker_flash_vol_rm_pre","Se está eliminando el volumen ");
+addS ("docker_flash_vol_rm_post"," en segundo plano. Esto puede tardar unos segundos...");
 
 
 //Common
@@ -116,6 +124,7 @@ addS ("docker_interface","Interfaz de red de Docker:");
 addS ("docker_title_containers_running", "Contenedores Docker en ejecución:");
 addS ("docker_title_containers_stopped", "Contenedores Docker parados:");
 addS ("docker_title_images", "Imágenes Docker disponibles:");
+addS ("docker_title_volume", "Volúmenes Docker disponibles:");
 addS ("docker_not_installed","Docker no está instalado.");
 addS ("docker_installed","Docker está instalado.");
 addS ("docker_button_install","Instalar Docker (Community Edition)");
