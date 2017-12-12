5bcd71d905cf6be3d8b3382abf159df380ceff1e
Roger Pueyo Centelles <roger.pueyo@guifi.net>
2017-12-12 10:44:48 +0100

    [errors] Format web interface on errors (fix #23)

--- web/lib/errors.php
+++ web/lib/errors.php
@@ -7,32 +7,32 @@ function notFunctionExist()
     $c = array_shift($Parameters);
     $a = array_shift($Parameters);
 
-    ErrorPage(1, 'notFunctionExist', "($c::$a): Function doesn't exist.");
+    ErrorPage(1, 'function_not_found', t('errors_function_pre')."($c::$a)".t('errors_function_post'));
 }
 
-function notFileExist($name)
+function notFileExist($name = "(unknown)")
 {
-    ErrorPage(2, 'notFileExist', "'$name': This file doesn't exist.");
+    ErrorPage(2, 'file_not_found', t('errors_file_pre').$name.t('errors_file_post'));
 }
 
-function notReadFile($name)
+function notReadFile($name = "(unknown)")
 {
-    ErrorPage(3, 'notReadFile', "'$name': Can't read this file.");
+    ErrorPage(3, 'file_not_readable', t('errors_read_pre').$name.t('errors_read_post'));
 }
 
-function notWriteFile($name)
+function notWriteFile($name = "(unknown)")
 {
-    ErrorPage(4, 'notWriteFile', "'$name': Can't write this file.");
+    ErrorPage(4, 'file_not_writeable', t('errors_write_pre').$name.t('errors_write_post'));
 }
 
-function callbackReturnUnknow($cmd)
+function callbackReturnUnknow($cmd = "(unknown)")
 {
-    ErrorPage(5, 'callbackReturnUnknow', "$cmd: Command callback return unknow.");
+    ErrorPage(5, 'unknown_callback_return', t('errors_callback_pre').$cmd.t('errors_callback_post'));
 }
 
-function errorExecuteExternalProgram($cmd, $output="")
+function errorExecuteExternalProgram($cmd = "(unknown)", $output = "(empty)")
 {
-    ErrorPage(5, 'errorExecuteExternalProgram', "$cmd: Error execute program.\n$output");
+    ErrorPage(5, 'errorExecuteExternalProgram', t('errors_command_pre').$cmd.t('errors_command_post').$output);
 }
 
 
@@ -45,15 +45,22 @@ function ErrorPage($knum, $kstr, $explain)
     require "templates/begincontent.php";
     require "templates/flash.php"; ?>
 
-	<div>
-		<h1>Error</h1>
-		<h2><?php echo $kstr."(".$knum.")" ?></h2>
-		<p><?php echo $explain; ?></p>
+    <div>
+		<h1><?php echo t("errors_error")?></h1>
+		<h2><?php echo t("errors_code").": ".$knum." (".$kstr.")" ?></h2>
+		<p><?php echo ptxt(print_r($explain, true)); ?></p>
+    <?php
+    echo addButton(array('label'=>t("errors_button_back"),'class'=>'btn btn-default', 'href'=>'javascript:history.back()'));
+    echo addButton(array('label'=>t("errors_button_home"),'class'=>'btn btn-primary', 'href'=>$staticPath)); ?>
+
 	</div>
-<?php
+
+    <?php
     require "templates/endcontent.php";
     require "templates/footer.php";
     require "templates/endpage.php";
+
+
 
     exit();
 }
